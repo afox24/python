0fecfcca3d151ed165290b3be2cc2c93260c1f83
Alisa Kirgizova <afox24@mail.ru>
2020-01-23 16:17:54 +0300

    update task 4

--- Lesson-1/4.py
+++ Lesson-1/4.py
@@ -1,7 +1,9 @@
-number = int(input('Введите число: '))
-x = 0
-while number < x:
-    print()
+number = input('Введите число: ')
+x_max = 0
+i = 0
+while i < len(number):
+    if int(number[i]) > x_max:
+        x_max = int(number[i])
+    i+=1
 
-
-print(x)
+print(x_max)

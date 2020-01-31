5942b05e6f1ad2fc2bc1b036f3bc2e245c6a53ac
Alisa Kirgizova <afox24@mail.ru>
2020-01-31 14:17:17 +0300

    New tasks lesson 3

---
+++ Lesson-3/1.py
@@ -0,0 +1,14 @@
+def my_func(x, y):
+    try:
+        z = x / y
+        return z
+    except ZeroDivisionError:
+        return "y не может быть нулем"
+    except ValueError:
+        return "введите число"
+print(my_func(int(input("Введите x = ")), int(input("Введите y = "))))
+
+
+
+
+
---
+++ Lesson-3/6.py
@@ -0,0 +1,6 @@
+def int_func():
+    word = input("Введите слова: ")
+    print(word.title())
+    return
+
+print(int_func())
